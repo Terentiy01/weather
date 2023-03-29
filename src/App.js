40cf0739e6deb42af54890fa0ecdd04984662fcd
Almaz <almazius11@gmail.com>
2023-03-29 13:28:10 +0300

    made the UI layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 function App() {
-  return <div></div>
+  return (
+    <div className="container">
+      <input placeholder="Введите город..." className="search" type="text" />
+      <div className="temp">
+        <h2>Город</h2>
+        <h1>28°C</h1>
+      </div>
+      <div className="dop">
+        <h3>Вероятность дождя</h3>
+        <h3>Влажность</h3>
+        <h3>Скорость ветра</h3>
+      </div>
+    </div>
+  )
 }
 
 export default App
